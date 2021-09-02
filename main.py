95ba64843205c4f0c0807a17ae6bf33f14efd412
Pedro Rodrigues <49986999+ophsr@users.noreply.github.com>
2021-09-02 11:02:45 -0300

    Update script python

--- main.py
+++ main.py
@@ -18,14 +18,16 @@ def log_config():
     root_logger.addHandler(handler)
 
  
-def runnig_message():
+def runnig_message(amount,currentItem):
     # for windows
     if name == 'nt':
         _ = system('cls')
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+    print(str(currentItem)+"/"+str(amount))
     print("Running...")
+    
 
 def finished_message():
     print("Log is: "+str(logging.getLogger("IpPhonesAlcaltelTelnet.log"))+"\nFINISHED!")
@@ -33,16 +35,16 @@ def finished_message():
 
 ## Give the location of the file and set array of the column, this method return this array
 def ips_from_excel_file():
-    # location = ("C:\Temp\MD5.xls")
-    location = ("/home/phsr/Documentos/Projects/ScriptsPython/IpPhonesAlcaltelTelnet/MD5.xls")
+    location = ("C:\Temp\MD5.xls")
+    #location = ("/home/phsr/Documentos/Projects/ScriptsPython/IpPhonesAlcaltelTelnet/MD5.xls")
 
     ## To open Workbook
     wb = xlrd.open_workbook(location)
     sheet = wb.sheet_by_index(0)
     
     ## Array with values(ip) from table
-    #arrayofvalues = sheet.col_values(1)
-    arrayofvalues = sheet.col_values(0)
+    arrayofvalues = sheet.col_values(1)
+    #arrayofvalues = sheet.col_values(0)
 
     logging.info('The amount IP address: '+ str(len(arrayofvalues)))
     logging.info('Location is '+location)
@@ -51,27 +53,69 @@ def ips_from_excel_file():
 
 def telnet_session(value_ip_host):
     #Open Telnet and command run
-    tn = telnetlib.Telnet(value_ip_host,23,10)
-    logging.info("Telnet session start in "+value_ip_host)
-
-    tn.write(b"dot1x tls on \n")
-    tn.write(b"reset \n")
+    port=23
+    timout=6
     
+    tn = telnetlib.Telnet(value_ip_host,port,timout)
+    logging.info("Telnet session start in "+value_ip_host)
+    
+    command2(value_ip_host,tls_status_command(value_ip_host,tn),port,timout) 
+
+def tls_status_command(ip,tn):
+
+    # Comnand for view tls status
+    tn.write(b"dot1x \n")    
+    tn.write(b"exit \n")
+     
     # Record all output and close session
-    tn.get_socket().shutdown(socket.SHUT_WR)
     data = tn.read_all().decode('ascii')
     out = data.splitlines()
-    tn.close()
     
-    #Print OutPut
-    print(data+"\n")
-    logging.info("Command response in ip: "+ value_ip_host - out[2])
+    
+    tn.get_socket().shutdown(socket.SHUT_WR)
+    tn.close()
+
+    logging.info("Command response in ip: "+ ip + " - " + out[3].strip().replace(" ", ""))
+
+
+    return out[3].strip().replace(" ", "")
+
+def command2(ip,data,port,timout,):
+    tn = telnetlib.Telnet(ip,port,timout)    
+
+    tls_status = data
+    if tls_status == "TLS:DISABLED":
+
+        tn.write(b"dot1x tls on \n")
+        tn.write(b"exit \n")
+
+        data_tls_on = tn.read_all().decode('ascii')
+        out = data_tls_on.splitlines()
+        
+        tn.get_socket().shutdown(socket.SHUT_WR)
+        tn.close()
+        
+        logging.info("Command response in ip: "+ ip + " - " + out[2])
+        reset_command(ip,port,timout)
+    else:
+        tn.get_socket().shutdown(socket.SHUT_WR)
+        tn.close()
+    
+
+def reset_command(ip,port,timout):
+    tn = telnetlib.Telnet(ip,port,timout)    
+    tn.write(b"reset \n")
+    logging.info("Reset command in ip: "+ ip)
+    tn.get_socket().shutdown(socket.SHUT_WR)
+    tn.close()
 
 
 def main():
     log_config()
-    for value in ips_from_excel_file():
-        runnig_message()
+    arrayOfIps = ips_from_excel_file()
+    currentItem=1
+    for value in arrayOfIps:
+        runnig_message(len(arrayOfIps),currentItem)
         if regexIP.match(str(value)):
             try:
                 telnet_session(value)
@@ -81,6 +125,7 @@ def main():
                 logging.error(str(e)+" in HOST: "+value)
         else:
             logging.warning("Not valid IP number: "+ str(value))
+        currentItem+=1
     finished_message()
 
 
